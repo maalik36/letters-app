a9674f3008728716565697ca49de8c2dfb669881
Maalik Brown <mbrown29@zagmail.gonzaga.edu>
2024-11-19 18:10:34 -0800

    started app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,30 @@ import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <h1>Home</h1>
+        h1.onclick = doSomething
+          <h2>Clipboard</h2>
+            <p>Paste</p>
+            <p>Cut</p>
+            <p>Copy</p>
+            <p>Format Painter</p>
+          <h2>Font</h2>
+            <p>Time New Roman</p>
+            <p>Bold</p>
+            <p>Italics</p>
+            <p>Underline</p>
+            <p>strikethrough</p>
+            <p>subscript</p>
+            <p>superscript</p>
+            <p>font size</p>
+      </header>
+    </div>
+  );                  /*
+  Edit <code>src/App.js</code> and save to reload.
+  <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -17,9 +34,12 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-    </div>
-  );
+        <img src={logo} className="App-logo" alt="logo" />
+                      <Headers>
+                        
+                        
+                    </Headers>
+                    */
 }
 
 export default App;
