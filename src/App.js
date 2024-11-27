bbfbb2f769a3124ed4f03c0897148d56802d1400
Maalik Brown <mbrown29@zagmail.gonzaga.edu>
2024-11-27 12:48:29 -0800

    can switch tabs and textarea added

--- src/App.js
+++ src/App.js
@@ -3,42 +3,72 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [display, setDisplay] = useState("gone");
-  function Appear()
+  const [home, setHome] = useState("gone");
+  const [insert, setInsert] = useState("gone");
+  const [bold, setBold] = useState("normal")
+  function Appear(word)
   {
     //let state = document.getElementById('sub');
     //state.style.display = "block";
-    console.log("yes");
-    alert("yes");
-    setDisplay("here");
+    if (word === "Home")
+    {
+      setHome("here");
+      setInsert("gone");
+    }
+    else if (word === "Insert")
+    {
+      setInsert("here");
+      setHome("gone");
+    }
+    
   };
+
+  function Bold() {
+    setBold("bold")
+  }
   return (
     <div className="App-header">
       <header>
-        <h1 onClick={() => Appear()}>Home</h1>
-        <div>
-          <h2 className={display}>Clipboard</h2>
-            <p className={display}>Paste</p>
-            <p className={display}>Cut</p>
-            <p className={display}>Copy</p>
-            <p className={display}>Format Painter</p>
-        </div>
-        <div>
-          <h2 className={display}>Font</h2>
-            <p className={display}>Time New Roman</p>
-            <p className={display}>Bold</p>
-            <p className={display}>Italics</p>
-            <p className={display}>Underline</p>
-            <p className={display}>strikethrough</p>
-            <p className={display}>subscript</p>
-            <p className={display}>superscript</p>
-            <p className={display}>font size</p>
-          </div>  
-          
+        <h1 onClick={() => Appear("Home")}>Home</h1>
+        <h1 onClick={() => Appear("Insert")}>Insert</h1>
       </header>
+      <box className={home} id="Clipboard">
+        
+            Paste
+            Cut
+            Copy
+            Format Painter
+      </box>  
+            <box className={home} id="Font">
+            Time New Roman
+            Bold
+            Italics
+            Underline
+            strikethrough
+            subscript
+            superscript
+            font size
+        
+      </box>  
+        
+            <box className={insert}>
+              Cover Page
+              Blank Page
+              Page Break
+            </box>
+              
+        
+      <header>
+        <h2 className={home}>Clipboard</h2>
+        <h2 className={home}>Font</h2>
+      </header>  
       
-       
+      
+      <textarea className={bold}>
+      
+      </textarea>   
     </div>
+    
   );
   
   /*
